ff7ee06dc4b16bea7d68d5a8e2451f1241ee2fa8
Sarvesh Bajaj <bt22cse087@iiitn.ac.in>
2024-09-03 20:13:00 +0530

    fixing 6

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -7,7 +7,12 @@ export const GET = async (request) => {
 
         const prompts = await Prompt.find({}).populate("creator");
 
-        return new Response(JSON.stringify(prompts), { status: 200 })
+        return new Response(JSON.stringify(prompts), {
+            status: 200,
+            headers: {
+                "Cache-Control": "no-store, max-age=0, must-revalidate",
+            },
+        });
     } catch (error) {
         return new Response("Failed to fetch all prompts",error, { status: 500 })
     }
